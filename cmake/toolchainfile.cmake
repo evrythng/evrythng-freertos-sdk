260b38e2bab1676ecd673759103c6370e12daa69
Ruslan Filipenko <ruslan.filipenko@evrythng.com>
2015-06-22 16:46:45 +0300

    remove 02 option from debug build

--- cmake/toolchainfile.cmake
+++ cmake/toolchainfile.cmake
@@ -2,6 +2,6 @@
 #set(CMAKE_C_COMPILER ${TOOLCHAIN_DIR}/usr/bin/x86_64-unknown-linux-gnu-gcc)
 #set(CMAKE_CXX_COMPILER ${TOOLCHAIN_DIR}/usr/bin/x86_64-unknown-linux-gnu-g++)
 
-set(EXTRA_FLAGS "-O2 -Wall")
+set(EXTRA_FLAGS "-Wall -Wno-unused-variable")
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${EXTRA_FLAGS}")
 #set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 ${EXTRA_FLAGS}" CACHE STRING "Buildroot CXXFLAGS" FORCE)
